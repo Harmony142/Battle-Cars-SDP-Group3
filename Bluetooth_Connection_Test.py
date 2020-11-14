4db99d3701f42e8d7107cdcc3f4ea29b984c69da
zwannie99 <43447822+zwannie99@users.noreply.github.com>
2020-11-13 20:22:05 -0500

    Bluetooth test update

--- Bluetooth_Connection_Test.py
+++ Bluetooth_Connection_Test.py
@@ -1,7 +1,10 @@
 import bluetooth
+from time import sleep
 
-#--Pair HC-05 with PC first
-
+# Pair HC-05 with PC first
+# TODO add try catch statement for OSError when board is powered off to auto-reconnect
+# TODO investigate multithreading/REST interfaces to allow streaming commands to multiple machines
+# TODO see if we can connect to HC-05's without passwords disabled, not vital
 target_name = "HC-05"
 target_address = None
 
@@ -9,39 +12,40 @@ nearby_devices = bluetooth.discover_devices()
 print(nearby_devices)
 
 for bdaddr in nearby_devices:
-    print (bluetooth.lookup_name( bdaddr ))
-    if target_name == bluetooth.lookup_name( bdaddr ):
+    print(bluetooth.lookup_name(bdaddr))
+    if target_name == bluetooth.lookup_name(bdaddr):
         target_address = bdaddr
         break
 
 if target_address is not None:
-    print ("found target bluetooth device with address ", target_address)
-    sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+    print("found target bluetooth device with address ", target_address)
+    sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 
-    print ("Trying connection")
-    #######################################
-    i=0 # ---- your port range starts here
-    maxPort = 3 # ---- your port range ends here
+    print("Trying connection")
+
+    i = 0  # Your port range starts here
+    maxPort = 3  # Your port range ends here
     err = True
-    while err == True and i <= maxPort:
-        print ("Checking Port ",i)
+    while err and i <= maxPort:
+        print("Checking Port ", i)
         port = i
         try:
             sock.connect((target_address, port))
             err = False
         except Exception as e:
-            ## print the exception if you like
-            print(Exception)
+            # Print the exception if you like
             i += 1
     if i > maxPort:
         print("Port detection Failed.")
         exit(0)
-    #######################################
-    print ("Trying sending")
-    sock.send("Challange")
-    print ("Finished sending")
+
+    print("Trying sending")
+    while True:
+        sock.send("Challenge\n")
+        sleep(4)
+    print("Finished sending")
     print(sock.recv(1024))
-    print ("Finished receiving")
+    print("Finished receiving")
     sock.close()
 else:
-    print ("could not find target bluetooth device nearby")
+    print("could not find target bluetooth device nearby")
