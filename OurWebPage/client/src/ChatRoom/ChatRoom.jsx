2e6a20ce918a105f5ac577c62fb32a4cb1ac8de5
achen173 <54816128+achen173@users.noreply.github.com>
2020-11-17 10:39:27 -0500

    remove service worker and update chatroom

--- OurWebPage/client/src/ChatRoom/ChatRoom.jsx
+++ OurWebPage/client/src/ChatRoom/ChatRoom.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import "./ChatRoom.css";
-import useChat from "../useChat";
+// import useChat from "../useChat";
 import Space from './space.jpg'
 
 const prev_keysPressed = {'w':false, 'a':false, 's':false, 'd':false};
 const curr_keysPressed = {'w':false, 'a':false, 's':false, 'd':false};
 
 const ChatRoom = (props) => {
-  const { roomId } = props.match.params;
-  const { messages, sendMessage } = useChat(roomId);
+  // const { roomId } = props.match.params;
+  // const { messages, sendMessage } = useChat(roomId);
   const [newMessage, setNewMessage] = React.useState("");
   
-  // const keysPressed = {'w':false, 'a':false, 's':false, 'd':false};
   document.addEventListener('keydown', e => curr_keysPressed[e.key] = true)
   document.addEventListener('keyup', e => curr_keysPressed[e.key] = false)
 
@@ -19,8 +18,10 @@ const ChatRoom = (props) => {
 
   const handleKeyPress = (event) => {
     if(JSON.stringify(prev_keysPressed) !== JSON.stringify(curr_keysPressed)){
-      sendMessage(JSON.stringify(curr_keysPressed));
-      setNewMessage("");
+      console.log("hello");
+      // sendMessage(JSON.stringify(curr_keysPressed));
+      // setNewMessage("");
+      console.log("HI");
       prev_keysPressed['w'] = curr_keysPressed['w'];
       prev_keysPressed['a'] = curr_keysPressed['a'];
       prev_keysPressed['s'] = curr_keysPressed['s'];
@@ -37,7 +38,7 @@ const ChatRoom = (props) => {
     <div  onKeyUp={handleKeyPress} onKeyDown={handleKeyPress}>
       <img className="room-page" src={Space}/>
       <iframe src="https://viewer.millicast.com/v2?streamId=hrFywT/kgplc3ye" allowFullScreen className="room-video"></iframe>
-      <h1 className="room-name">Name: {roomId}</h1>
+      {/* <h1 className="room-name">Name: {roomId}</h1> */}
       <textarea
         value={newMessage}
         onChange={handleNewMessageChange}
