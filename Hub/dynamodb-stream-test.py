e46cbdb723a5af732f9c1a21d0cfe6c8508cb00b
Logan Kuehl <logankhl361@gmail.com>
2020-11-18 19:59:04 -0500

    Enabled Stream Control
    The hub is able to read controls coming from the web server

--- Hub/dynamodb-stream-test.py
+++ Hub/dynamodb-stream-test.py
@@ -5,6 +5,7 @@ import boto3
 import csv
 import time
 import pprint
+import json
 pp = pprint.PrettyPrinter(indent=4)
 
 table_name = 'Todo-fkgtez5rpfcoferxahyyjer5i4-dev'
@@ -22,25 +23,27 @@ client = boto3.client(
     aws_secret_access_key=credentials['Secret access key'])
 
 # Find the correct stream ARN
+print('Attempting to connect to table: ', table_name)
 response = client.list_streams(
     TableName=table_name
 )
-pp.pprint(response['Streams'])
+# pp.pprint(response['Streams'])
 stream_arn = None
 for stream in response['Streams']:
     if stream['TableName'] == table_name:
         stream_arn = stream['StreamArn']
         break
 if stream_arn is None:
-    raise ValueError('Table name {} not found in current account'.format(table_name))
-pp.pprint(stream_arn)
+    raise ValueError('Table name {} not found in current account or no streams available'.format(table_name))
+print('Found stream: ', stream_arn)
 
 # Get the latest shard
 response = client.describe_stream(
     StreamArn=stream_arn
 )
-pp.pprint(response['StreamDescription']['Shards'])
-shard_id = response['StreamDescription']['Shards'][0]['ShardId']
+# pp.pprint(response['StreamDescription'])
+# TODO: figure out why the hell this is 2
+shard_id = response['StreamDescription']['Shards'][2]['ShardId']
 
 # Get the first shard iterator of the latest shard
 response = client.get_shard_iterator(
@@ -48,16 +51,20 @@ response = client.get_shard_iterator(
     ShardId=shard_id,
     ShardIteratorType='LATEST',
 )
-pp.pprint(response['ShardIterator'])
+# pp.pprint(response['ShardIterator'])
 shard_iterator = response['ShardIterator']
+print('Shard iterator: ', shard_iterator)
 
 while True:
     response = client.get_records(
         ShardIterator=shard_iterator
         # Limit=1
     )
-    time.sleep(1)
+    # pp.pprint(response['Records'])
+    if 'Records' in response.keys():
+        if len(response['Records']) > 0:
+            keys_pressed = json.loads(response['Records'][0]['dynamodb']['NewImage']['description']['S'])
+            # pp.pprint(keys_pressed)
+
     if 'NextShardIterator' in response.keys():
-        pp.pprint(response['Records'])
-        pp.pprint(response['NextShardIterator'])
         shard_iterator = response['NextShardIterator']
--- Hub/hub.py
+++ Hub/hub.py
@@ -3,6 +3,9 @@ import bluetooth
 from time import sleep
 import keyboard
 from inputs import devices
+import boto3
+import csv
+import json
 
 """
 ----- TO INSTALL PYBLUEZ ON WINDOWS -----
@@ -21,6 +24,146 @@ git clone https://github.com/pybluez/pybluez
 cd pybluez
 <conda env file>/python.exe setup.py install
 """
+
+
+def read_keyboard_commands():
+    cmd_flags = 0x00
+    if keyboard.is_pressed('s'):
+        cmd_flags |= 1 << 4
+    if keyboard.is_pressed('w'):
+        cmd_flags |= 1 << 5
+    if keyboard.is_pressed('a'):
+        cmd_flags |= 1 << 6
+    if keyboard.is_pressed('d'):
+        cmd_flags |= 1 << 7
+
+    if keyboard.is_pressed('shift'):
+        cmd_flags |= 0b1111
+
+    return cmd_flags
+
+
+def read_controller_commands():
+    cmd_flags = 0x00
+
+    # Try to find a controller if plugged in
+    events = devices.gamepads[0].read()
+    for event in events:
+        # Ignore sync messages, no interesting information
+        if event.ev_type == 'Sync':
+            continue
+
+        # Interpret buttons
+        if event.code == 'BTN_NORTH':
+            cmd_flags |= event.state << 0
+        elif event.code == 'BTN_EAST':
+            cmd_flags |= event.state << 1
+        elif event.code == 'BTN_SOUTH':
+            cmd_flags |= event.state << 2
+        elif event.code == 'BTN_WEST':
+            cmd_flags |= event.state << 3
+
+        # Interpret d-pad
+        elif event.code == 'ABS_HAT0Y':
+            event.state = 2 if event.state < 0 else event.state
+            cmd_flags |= event.state << 4
+        elif event.code == 'ABS_HAT0X':
+            event.state = 2 if event.state < 0 else event.state
+            cmd_flags |= event.state << 6
+
+        # Interpret left joystick
+        elif event.code == 'ABS_Y':
+            if event.state > 4000:
+                cmd_flags |= 1 << 5
+            elif event.state < -4000:
+                cmd_flags |= 1 << 4
+        elif event.code == 'ABS_X':
+            if event.state > 4000:
+                cmd_flags |= 1 << 7
+            elif event.state < -4000:
+                cmd_flags |= 1 << 6
+
+    return cmd_flags
+
+
+def read_database_commands(cl, it):
+    cmd_flags = None
+
+    response = cl.get_records(
+        ShardIterator=it
+        # Limit=1
+    )
+    # pp.pprint(response['Records'])
+    if 'NextShardIterator' in response.keys():
+        shard_iterator = response['NextShardIterator']
+
+    if 'Records' in response.keys() and len(response['Records']) > 0:
+        keys_pressed = json.loads(response['Records'][0]['dynamodb']['NewImage']['description']['S'])
+
+        cmd_flags = 0x00
+
+        if keys_pressed['KeyS']:
+            cmd_flags |= 1 << 4
+        if keys_pressed['KeyW']:
+            cmd_flags |= 1 << 5
+        if keys_pressed['KeyA']:
+            cmd_flags |= 1 << 6
+        if keys_pressed['KeyD']:
+            cmd_flags |= 1 << 7
+
+        if keys_pressed['ShiftLeft']:
+            cmd_flags |= 0b1111
+
+    return cmd_flags
+
+
+# Variables for connecting to the database for streaming commands from the web page
+# TODO make this robust it just connects once when you launch hub.py
+table_name = 'Todo-fkgtez5rpfcoferxahyyjer5i4-dev'
+credentials_csv_file_name = 'dynamodb-stream-readonly-creds.csv'
+
+with open(credentials_csv_file_name, newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    credentials = next(reader)
+
+# Connect to AWS DynamoDB API
+client = boto3.client(
+    service_name='dynamodbstreams',
+    region_name='us-east-2',
+    aws_access_key_id=credentials['Access key ID'],
+    aws_secret_access_key=credentials['Secret access key'])
+
+# Find the correct stream ARN
+print('Attempting to connect to table: ', table_name)
+response = client.list_streams(
+    TableName=table_name
+)
+stream_arn = None
+for stream in response['Streams']:
+    if stream['TableName'] == table_name:
+        stream_arn = stream['StreamArn']
+        break
+if stream_arn is None:
+    raise ValueError('Table name {} not found in current account or no streams available'.format(table_name))
+print('Found stream: ', stream_arn)
+
+# Get the latest shard
+response = client.describe_stream(
+    StreamArn=stream_arn
+)
+shard_id = response['StreamDescription']['Shards'][2]['ShardId']
+
+# Get the first shard iterator of the latest shard
+response = client.get_shard_iterator(
+    StreamArn=stream_arn,
+    ShardId=shard_id,
+    ShardIteratorType='LATEST',
+)
+# pp.pprint(response['ShardIterator'])
+shard_iterator = response['ShardIterator']
+print('Shard iterator: ', shard_iterator)
+
+
 # TODO test if updates only happen on state change
 # TODO test if boost bug is fixed
 # TODO test if WASD bug is fixed
@@ -90,58 +233,13 @@ while True:
             '''
             while True:
                 command_flags = 0x00
-
                 try:
-                    # Try to find a controller if plugged in
-                    events = devices.gamepads[0].read()
-                    for event in events:
-                        # Ignore sync messages, no interesting information
-                        if event.ev_type == 'Sync':
-                            continue
-
-                        # Interpret buttons
-                        if event.code == 'BTN_NORTH':
-                            command_flags |= event.state << 0
-                        elif event.code == 'BTN_EAST':
-                            command_flags |= event.state << 1
-                        elif event.code == 'BTN_SOUTH':
-                            command_flags |= event.state << 2
-                        elif event.code == 'BTN_WEST':
-                            command_flags |= event.state << 3
-
-                        # Interpret d-pad
-                        elif event.code == 'ABS_HAT0Y':
-                            event.state = 2 if event.state < 0 else event.state
-                            command_flags |= event.state << 4
-                        elif event.code == 'ABS_HAT0X':
-                            event.state = 2 if event.state < 0 else event.state
-                            command_flags |= event.state << 6
-
-                        # Interpret left joystick
-                        elif event.code == 'ABS_Y':
-                            if event.state > 4000:
-                                command_flags |= 1 << 5
-                            elif event.state < -4000:
-                                command_flags |= 1 << 4
-                        elif event.code == 'ABS_X':
-                            if event.state > 4000:
-                                command_flags |= 1 << 7
-                            elif event.state < -4000:
-                                command_flags |= 1 << 6
-
+                    # Raises index error if a controller is not found
+                    command_flags = read_controller_commands()
                 except IndexError:
                     # Read keyboard commands since we couldn't find a controller
-                    if keyboard.is_pressed('s'):
-                        command_flags |= 1 << 4
-                    if keyboard.is_pressed('w'):
-                        command_flags |= 1 << 5
-                    if keyboard.is_pressed('a'):
-                        command_flags |= 1 << 6
-                    if keyboard.is_pressed('d'):
-                        command_flags |= 1 << 7
-
-                    if keyboard.is_pressed('shift'):
-                        command_flags |= 0b1111
+                    command_flags, shard_iterator = read_database_commands(client, shard_iterator)
+                    command_flags |= read_keyboard_commands()
 
                 # Boost logic
                 if boost_tank < boost_tank_max_capacity:
@@ -160,7 +258,7 @@ while True:
 
                     # Limit how fast we can send updates to once every 10th of a second
                     # as not to overwhelm the receiver's message buffer
-                    sleep(.1)
+                    # sleep(.1) TODO check if we should do something like this
 
         else:
             print("Failed to find target bluetooth device nearby")
