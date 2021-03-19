56ed45db0074ab52a9dde6f44b8e0329f8fa9910
Logan Kuehl <logankhl361@gmail.com>
2021-03-19 14:04:03 -0400

    Website Conversion Test
    Swapped the website to use SQS instead of DynamoDBStreams. This is the first test to see if it will build on AWS and if it will read the credentials and url from the Amplify environment variables properly

--- WebPage/src/ChatRoom/ChatRoom.jsx
+++ WebPage/src/ChatRoom/ChatRoom.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 import "./ChatRoom.css";
 import Space from './space.jpg';
-import { API } from 'aws-amplify';
-import { createTodo, updateTodo } from '../graphql/mutations';
+// import { API } from 'aws-amplify';
+// import { createTodo, updateTodo } from '../graphql/mutations';
 import CustomizationMenu from './Customization.jsx'
 
 // updateTodo
 const prev_keysPressed = {'KeyW':false, 'KeyA':false, 'KeyS':false, 'KeyD':false, 'ShiftLeft':false};
 const curr_keysPressed = {'KeyW':false, 'KeyA':false, 'KeyS':false, 'KeyD':false, 'ShiftLeft':false};
-const formData = {id: 1234, name: "hello", description: ""}
-
+const formData = {id: 1234, name: "hello", description: ""};
 const allowed_keys = Object.keys(curr_keysPressed);
 // console.log(JSON.stringify(allowed_keys));
+
+var AWS = require('aws-sdk');
+var sqs = new AWS.SQS({
+    accessKeyId: 'AKIAY563PRYUWD457KGQ',
+    secretAccessKey: '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'
+});
 
 document.addEventListener('keydown', e => {if(allowed_keys.includes(e.code)){curr_keysPressed[e.code] = true;}});
 document.addEventListener('keyup', e => {if(allowed_keys.includes(e.code)){curr_keysPressed[e.code] = false;}});
 
 document.body.style.overflow = "hidden";
+
+function uuidv4() {
+  // From https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
 
 const ChatRoom = (props) => {
   const [newMessage, setNewMessage] = React.useState("");
@@ -34,8 +47,21 @@ const ChatRoom = (props) => {
     payload['StartTime'] = d.getTime();
     formData.description = JSON.stringify(payload);
     console.log(formData);
-    API.graphql({ query: createTodo, variables: { input: formData } })
-        .catch(e => {API.graphql({ query: updateTodo, variables: { input: formData } });});
+    var params = {
+      MessageDeduplicationId: uuidv4(),  // Required for FIFO queues
+      MessageGroupId: "car-1",  // Required for FIFO queues
+      MessageBody: JSON.stringify(payload),
+      QueueUrl:process.env.SQS_URL
+    }
+    sqs.sendMessage(params, function(err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data.MessageId);
+      }
+    });
+    // API.graphql({ query: createTodo, variables: { input: formData } })
+    //    .catch(e => {API.graphql({ query: updateTodo, variables: { input: formData } });});
   }
 
   const handleNewMessageChange = (event) => {
