39c55fabb513301dad3f84bac05175285605efd1
Logan Kuehl <logankhl361@gmail.com>
2021-04-21 16:48:01 -0400

    Graph Update
    Separated the graph into two separate graphs, one for end-to-end delays and one for in-system delays

--- Hub/analysis.py
+++ Hub/analysis.py
@@ -11,24 +11,32 @@ if __name__ == '__main__':
 
     # Plot the points and their error bars
     thickness = 2
-    plt.errorbar(message_delays, sample_means[0], yerr=z_score * standard_errors[0],
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(16, 9))
+    ax1.errorbar(message_delays, sample_means[0], yerr=z_score * standard_errors[0],
                  fmt='o', markersize=thickness * 3, capsize=thickness * 2, capthick=thickness,
                  label='End-to-End Delay', linewidth=thickness, color='red')
-    plt.errorbar(message_delays, sample_means[1], yerr=z_score * standard_errors[1],
+    ax2.errorbar(message_delays, sample_means[1], yerr=z_score * standard_errors[1],
                  fmt='o', markersize=thickness * 3, capsize=thickness * 2, capthick=thickness,
                  label='In-System Delay', linewidth=thickness, color='blue')
 
     # Add a horizontal line for 60 ms
-    plt.plot(message_delays, numpy.zeros(len(message_delays)) + 60,
-             label='60ms Benchmark', linewidth=thickness, color='green')
+    benchmark = 60
+    edge_spacing = 15
+    ax1.plot([starting_delay + edge_spacing, ending_delay - edge_spacing], [60, 60],
+             label='60ms Benchmark', linestyle='--', linewidth=thickness, color='green')
+    ax2.plot([starting_delay + edge_spacing, ending_delay - edge_spacing], [60, 60],
+             label='60ms Benchmark', linestyle='--', linewidth=thickness, color='green')
 
     # Set the axis. Reversing the x-axis to show decrease in time
-    edge_spacing = 15
-    plt.axis(xmin=starting_delay + edge_spacing, xmax=ending_delay - edge_spacing, ymin=0)
-    plt.title('Average Response Time for {} Messages'.format(n))
-    plt.xlabel('Time Between Messages (ms)')
-    plt.ylabel('Average Response Time (ms)')
-    plt.grid(True, linewidth=1)
-    plt.legend()
+    ax1.axis(xmin=starting_delay + edge_spacing, xmax=ending_delay - edge_spacing, ymin=0)
+    ax2.axis(xmin=starting_delay + edge_spacing, xmax=ending_delay - edge_spacing, ymin=0)
+    fig.suptitle('Average Response Time for {} Messages'.format(n), fontsize=32)
+    ax1.set_title('End-to-End Delay')
+    ax2.set_title('In-System Delay')
+    ax1.set(xlabel='Time Between Messages (ms)', ylabel='Average Response Time (ms)')
+    ax2.set(xlabel='Time Between Messages (ms)', ylabel='Average Response Time (ms)')
+    ax1.grid(True, linewidth=1)
+    ax2.grid(True, linewidth=1)
+
     plt.savefig(graph_file_path)
     plt.show()
